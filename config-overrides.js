d28ba1427abbae2178cafc3e38fe06f6a030fe33
Jordan Morris <jtmorrisbytes@gmail.com>
2021-01-11 00:21:57 -0600

    webassembly now works with webpack 4 builtin support

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
+const { default: WasmPackPlugin } = require("@wasm-tool/wasm-pack-plugin");
 const path = require("path");
 
 module.exports = function override(config, env) {
   const wasmExtensionRegExp = /\.wasm$/;
 
-  config.resolve.extensions.push(".wasm");
+  // config.resolve.extensions.push(".wasm");
 
   config.module.rules.forEach((rule) => {
     (rule.oneOf || []).forEach((oneOf) => {
@@ -15,13 +16,29 @@ module.exports = function override(config, env) {
   });
 
   // add a dedicated loader for WASM
-  config.experiments = { asyncWebAssembly: true };
+  // config.experiments = { asyncWebAssembly: true };
+  // config.module.rules.push({
+  //   test: wasmExtensionRegExp,
+  //   include: [path.resolve(__dirname, "src/shared/")],
+  //   type: "javascript/auto",
+  //   use: [
+  //     {
+  //       loader: require.resolve("raw-loader"),
+  //       // options: { export: "instance" },
+  //     },
+  //   ],
+  // });
   config.module.rules.push({
     test: wasmExtensionRegExp,
-    include: [path.resolve(__dirname, "src/shared/")],
     type: "webassembly/experimental",
-    use: [{ loader: require.resolve("wasm-loader"), options: {} }],
   });
-
+  // config.plugins(
+  //   new WasmPackPlugin({
+  //     crateDirectory: path.resolve(__dirname, "src/shared/decimalS"),
+  //   })
+  // );
+  // config.experiments = {
+  //   asyncWebAssembly: true,
+  // };
   return config;
 };
